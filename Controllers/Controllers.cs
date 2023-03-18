c67e6ef93a30e60fd356e40a56ba4f989451e785
Richard Anthony Hein <40005875+auxon@users.noreply.github.com>
2023-03-18 21:11:53 +0000

    Initial project creation

--- Controllers/Controllers.cs
+++ Controllers/Controllers.cs
@@ -1,6 +1,4 @@
 using Microsoft.AspNetCore.Mvc;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using TutorTracker.Data;
 
--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -10,15 +10,22 @@ namespace TutorTracker.Data
         }
 
         // Define your entity sets and their properties here
-      public DbSet<Tutor> Tutors { get; set; }
-      public DbSet<Appointment> Appointments { get; set; }
+        public DbSet<Tutor> Tutors { get; set; } = null!;
+        public DbSet<Appointment> Appointments { get; set; } = null!;
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-          modelBuilder.Entity<Tutor>().HasKey(t => t.Id);
+            modelBuilder.Entity<User>().HasKey(u => u.Id);
+            modelBuilder.Entity<User>().Property(u => u.Email).IsRequired();
+            modelBuilder.Entity<User>().Property(u => u.FirstName).IsRequired();
+            modelBuilder.Entity<User>().Property(u => u.LastName).IsRequired();
+            modelBuilder.Entity<User>().Property(u => u.Username);
+          
+            modelBuilder.Entity<Tutor>().HasKey(t => t.Id);
             modelBuilder.Entity<Tutor>().Property(t => t.User).IsRequired();
             modelBuilder.Entity<Tutor>().Property(t => t.HourlyRate).IsRequired();
             modelBuilder.Entity<Tutor>().Property(t => t.Subject).IsRequired();
+          
             modelBuilder.Entity<Appointment>().HasKey(a => a.Id);
             modelBuilder.Entity<Appointment>().Property(a => a.Tutor).IsRequired();
             modelBuilder.Entity<Appointment>().Property(a => a.Student).IsRequired();
